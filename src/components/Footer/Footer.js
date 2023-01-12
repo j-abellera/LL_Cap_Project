cf0bd92cd61237541bcccf5aa7472dac5163fb55
Justin Abellera <justin.c.abellera@gmail.com>
2023-01-11 22:20:17 -0800

    wireframe footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,37 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className='footer-container'>
-            <div>Footer</div>
+            <div className='footer-image'>Image Goes Here</div>
+            <div className='footer-nav'>
+                <h4 className='fnav-title'>Navigation</h4>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            <div className='footer-contact'>
+                <h4 className='contact-title'>Contact</h4>
+                <div className='address'>
+                    <address>
+                        1234 Meringue Way
+                        <br/>Spokane, WA 19340
+                        <br/><br/>tel. 661-555-7563
+                        <br/><br/>contact@littlelemon.com
+                    </address>
+                </div>
+            </div>
+            <div className='footer-social'>
+                <h4 className='social-title'>Social</h4>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>TikTok</li>
+                </ul>
+            </div>
         </footer>
     )
 }
