0ab87478e5b0a17e2177bd865189f1892007e413
Justin Abellera <justin.c.abellera@gmail.com>
2023-01-12 00:27:24 -0800

    hifi about section

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -5,13 +5,13 @@ function About() {
     return (
         <div className='about-container'>
             <div className='about-left'>
-                <h1 className='about-title'>Little Lemon</h1>
-                <h2 className='about-sub'>Sub Text</h2>
-                <p className='about-description'>about description</p>
+                <h2 className='about-title'>Little Lemon</h2>
+                <h3 className='about-sub'>Chicago</h3>
+                <p className='about-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare lectus elit, a luctus sem mollis sit amet. Donec et nisi auctor, porta ante at, porttitor neque. Morbi lobortis ultricies dictum. Etiam mollis arcu dui, non fringilla quam lacinia vel. Nam dapibus fermentum urna. Aenean congue feugiat volutpat.</p>
             </div>
             <div className='about-image'>
-                <div className='about-img1'>Image1</div>
-                <div className='about-img2'>Image2</div>
+                <div className='about-img1'></div>
+                <div className='about-img2'></div>
             </div>
         </div>
     )
