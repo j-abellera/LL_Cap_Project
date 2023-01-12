9d15dae7fa2f1980aaf5b1826c3aaf03bca02aea
Justin Abellera <justin.c.abellera@gmail.com>
2023-01-11 22:50:16 -0800

    hifi navigation section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <div id='container'>
       <head>
         <title>Little Lemon</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Karla&family=Markazi+Text:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+        <style> @import url('https://fonts.googleapis.com/css2?family=Karla&family=Markazi+Text:wght@400;500;600;700&display=swap'); </style>
         <meta
           name="description"
           content="Little Lemon offers traditional cuisine in a lemon. That's right, if you're reading this, you know what I just said made no sense."
