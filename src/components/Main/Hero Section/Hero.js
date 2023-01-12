677c7fe96fd4dd6443061d2187a786107d85b566
Justin Abellera <justin.c.abellera@gmail.com>
2023-01-11 23:23:29 -0800

    hifi hero section

--- src/components/Main/Hero Section/Hero.js
+++ src/components/Main/Hero Section/Hero.js
@@ -5,13 +5,15 @@ function Hero() {
     return (
         <div className='hero-container'>
             <div className='hero-left'>
-                <h1 className='hero-title'>TITLE Little Lemon</h1>
-                <h2 className='hero-sub'>SUB Chicago</h2>
-                <p className='hero-text'>TEXT Some text about little lemon</p>
-                <p className='hero-button'>BUTTON "Reserve Table"</p>
+                <h1 className='hero-title'>Little Lemon</h1>
+                <h2 className='hero-sub'>Chicago</h2>
+                <p className='hero-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare lectus elit, a luctus sem mollis sit amet. Donec et nisi auctor, porta ante at, porttitor neque. Morbi lobortis ultricies dictum. Etiam mollis arcu dui, non fringilla quam lacinia vel.</p>
+                <div className='hero-button'>
+                    <h4>Reserve Table</h4>
+                </div>
             </div>
             <div className='hero-right'>
-                <div className='hero-image'>Image container</div>
+                <div className='hero-image'></div>
             </div>
         </div>
     )
